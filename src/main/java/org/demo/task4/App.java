643d78a20f8ba2159383897b3cd07f5439dfb95c
Grazia Ippolito <graziaippolito@MacBook-Pro-3.local>
2018-12-03 22:50:51 +0100

    improved task 4

--- src/main/java/org/demo/task4/App.java
+++ src/main/java/org/demo/task4/App.java
@@ -14,36 +14,11 @@
 
         public static void displayResultsOfLogicalOperations(boolean a, boolean b){
 
-            boolean checkResult;
 
-            //OR
-            if (a || b)  {
-                checkResult = true;
-                System.out.println("argument #1 OR argument #2 " + checkResult);
-            } else  {
-                checkResult = false;
-                System.out.println("argument #1 OR argument #2 " + checkResult);
-            }
-
-            //AND
-            if (a && b)  {
-                checkResult = true;
-                System.out.println("argument #1 AND argument #2 " + checkResult);
-            } else {
-                checkResult = false;
-                System.out.println("argument #1 AND argument #2 " + checkResult);
-            }
-
-            //XOR
-            if (a ^ b)  {
-                checkResult = true;
-                System.out.println("argument #1 XOR argument #2 " + checkResult);
-            } else {
-                checkResult = false;
-                System.out.println("argument #1 XOR argument #2 " + checkResult);
-            }
-            System.out.println("////////////////////////");
+            System.out.println("argument #1 OR argument #2 " + (a || b));
+            System.out.println("argument #1 AND argument #2 " + (a && b));
+            System.out.println("argument #1 XOR argument #2 " + (a ^ b));
+            System.out.println("-------------------");
         }
-
 
     }
